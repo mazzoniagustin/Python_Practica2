9b7b7253ce1ddcaaa652fc77344bbbfb18f0a940
mazzoniagustin <mazzoni.agustine@gmail.com>
2025-04-02 15:41:53 -0300

    Se agregó la funcionalidad para ordenar el puntaje por ronda.

--- src/functions.py
+++ src/functions.py
@@ -19,13 +19,17 @@ def calculate_score (rounds):
             scores[player] = scores.get(player,0) + score     
             
         
-        mvp = max(round_score, key=round_score.get)
+        descendant_players = sorted(round_score.keys(), key= lambda player: round_score[player],reverse=True) 
+        
+        
+        mvp = descendant_players[0] # El MVP siempre va a ser el primero al estar ordenada.
         mvp_counter[mvp] = mvp_counter.get(mvp,0) + 1
         
-        for player in round_score:
+        
+        for player in descendant_players:
             score = round_score[player]
-            
             print (f'{player}: {score} puntos.')
+            
         print(f'MVP de la ronda: {mvp} con {round_score[mvp]} puntos.')
         
     return scores, mvp_counter
@@ -36,11 +40,12 @@ def show_ranking(scores, mvp_counter):
     print ('Jugador | Puntos | MVPs')
     print ('-------------------------')
     
+    # Se ordena de menor a mayor la acumulada.
     descendant_players = sorted(scores.keys(), key= lambda player: scores[player],reverse=True) 
     
     for player in descendant_players:
         points = scores[player]
         mvps = mvp_counter.get(player,0)
-        print(f'{player:<9} | {points} | {mvps}')
+        print(f'{player:<7} | {points} | {mvps}') 
     
     
